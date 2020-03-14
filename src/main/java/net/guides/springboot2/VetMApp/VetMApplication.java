f21504c55f68fb6f48c86b8bace5a84dc80f78fb
Jjiro <email@email.com>
2020-03-14 19:09:41 +0200

    ...

---
+++ src/main/java/net/guides/springboot2/VetMApp/VetMApplication.java
@@ -0,0 +1,17 @@
+package net.guides.springboot2.VetMApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetMApplication.class, args);
+
+
+	}
+
+
+
+}
---
+++ src/test/java/net/guides/springboot2/VetMApp/VetMApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.springboot2.VetMApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
